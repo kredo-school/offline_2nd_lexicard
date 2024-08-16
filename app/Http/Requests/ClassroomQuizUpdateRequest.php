39364c5f70236d190d9255603f8fd7759523b333
Shinsaku-Noto <shinsaku32823@gmail.com>
2024-08-16 15:07:38 +0800

    backend validation edit

--- app/Http/Requests/ClassroomQuizUpdateRequest.php
+++ app/Http/Requests/ClassroomQuizUpdateRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ClassroomRequest extends FormRequest
+class ClassroomQuizUpdateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,7 +22,7 @@ class ClassroomRequest extends FormRequest
     public function rules(): array
     {
         return [
-            
+            'title' => ['required','max:255'],
         ];
     }
 }
